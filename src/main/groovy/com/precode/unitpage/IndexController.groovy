f9741e7ac2019c70a3284cc1867b0fc4f979a63b
kimsonghee <kimsonghee@naver.com>
2018-04-10 14:26:08 +0900

    gradle setting2

--- src/main/groovy/com/precode/unitpage/IndexController.groovy
+++ src/main/groovy/com/precode/unitpage/IndexController.groovy
@@ -1,14 +1,14 @@
-package com.example.demo
+package com.precode.unitpage
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 
+
 @Controller
 class IndexController {
 
-	
 	@RequestMapping("/")
-	public String  djfk() {
+	public String index() {
 		return "index"
 	}
 }
