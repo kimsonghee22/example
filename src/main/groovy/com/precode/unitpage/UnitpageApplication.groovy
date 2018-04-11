29438482578c10c9234feac96bfc805e46a75cfe
kimsonghee <kimsonghee@naver.com>
2018-04-11 18:06:16 +0900

    start

--- src/main/groovy/com/precode/unitpage/UnitpageApplication.groovy
+++ src/main/groovy/com/precode/unitpage/UnitpageApplication.groovy
@@ -2,11 +2,21 @@ package com.precode.unitpage
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-class UnitpageApplication {
+@ComponentScan(basePackages = "com.precode.unitpage")
+class UnitpageApplication extends SpringBootServletInitializer{
 
 	static void main(String[] args) {
-		SpringApplication.run UnitpageApplication, args
+		SpringApplication.run (UnitpageApplication, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		application.sources(UnitpageApplication)
+	}
+	
 }
