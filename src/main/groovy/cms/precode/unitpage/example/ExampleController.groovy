8e9c265ed032a1c4d7b81edfa6dc527ebed4a297
kimsonghee <kimsonghee@naver.com>
2018-04-12 17:44:09 +0900

    html

--- src/main/groovy/cms/precode/unitpage/example/ExampleController.groovy
+++ src/main/groovy/cms/precode/unitpage/example/ExampleController.groovy
@@ -10,28 +10,32 @@ class ExampleController {
 	Sql sql = new Sql();
 	
 	
-	public String select(@RequestParam Map params, Model model) {
-		
+	String select(@RequestParam Map params, Model model) {
 		List list = sql.query("select * from board where userId= $params.userId")
 		
 		model.addAttribute("list", list)
 		return "example/list"
 	}
-	 
-	public String selectOneData(@RequestParam Map params, Model model) {
-		
+	
+	
+	String selectOneData(@RequestParam Map params, Model model) {
 		Map map = sql.row("select * from board where userId= $params.userId")
 		
 		model.addAttribute("map", map)
 		return "example/map"
 	}
 	
-	public void update(@RequestParam Map params) {
+	void update(@RequestParam Map params) {
 		sql.execute("update board set url= $params.url where id=$params.id")
 	}
-	
-	public void delete(@RequestParam Map params) {
+
+		
+	void delete(@RequestParam Map params) {
 		sql.execute("delete from file where fileName=$params.fileName")
+	}
+	
+	void insert(@RequestParam Map params) {
+		sql.execute("insert into board (title) values ($params.title)")
 	}
 
 	
