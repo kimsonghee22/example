2d4459ecfde07d4374eeae6cc2cdf2990ce7f9ab
Sherly <@none>
2018-04-14 01:31:16 +0900

    - Gradle Set up / Database Set up

--- src/main/groovy/com/precode/unitpage/UnitpageApplication.groovy
+++ src/main/groovy/com/precode/unitpage/UnitpageApplication.groovy
@@ -3,12 +3,12 @@ package com.precode.unitpage
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.boot.web.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.precode.unitpage")
-class UnitpageApplication extends SpringBootServletInitializer{
+class UnitpageApplication extends SpringBootServletInitializer {
 
 	static void main(String[] args) {
 		SpringApplication.run (UnitpageApplication, args);
