796f582c17cfaacb2cc228c3e7391da44db2dea1
Sherly <@none>
2018-04-19 21:24:15 +0900

    레이아웃 제작 중 - 헤더 초안 작업 - 프리마커 인덱스 테스트

--- src/main/groovy/com/precode/unitpage/IndexController.groovy
+++ src/main/groovy/com/precode/unitpage/IndexController.groovy
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 @Controller
 class IndexController {
 
-	@RequestMapping("/index1")
+	@RequestMapping("/index")
 	public String index() {
 		return "index"
 	}
