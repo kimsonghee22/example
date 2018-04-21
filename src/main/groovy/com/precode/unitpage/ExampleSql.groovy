ac58f17bb891dd215d18cd38a7ee527ee2dcd842
Sherly <@none>
2018-04-21 16:48:57 +0900

    - SQL 서비스 파트 세팅

--- src/main/groovy/com/precode/unitpage/ExampleSql.groovy
+++ src/main/groovy/com/precode/unitpage/ExampleSql.groovy
@@ -1,18 +1,18 @@
 package com.precode.unitpage
 
+import groovy.sql.Sql;
 import org.springframework.stereotype.Component
 
 @Component
 class ExampleSql {
+
 	def url = 'jdbc:mysql://qoov.iptime.org:33306/example'
 	def user = 'root'
 	def password = '51158128'
-	//def driver = 'com.mysql.jdbc.Driver'
-	def driver = 'net.sf.log4jdbc.sql.jdbcapi.DriverSpy'
-	
-	
+	def driver = 'com.mysql.jdbc.Driver'
+
 	List query( query ) {
-		def sql = ExampleSql.newInstance(url, user, password, driver)
+		def sql = Sql.newInstance(url, user, password, driver)
 		List result = sql.rows(query)
 		sql.close()
 		if(! result) {
@@ -22,7 +22,7 @@ class ExampleSql {
 	}
 
 	Map row(query) {
-		def sql = ExampleSql.newInstance(url, user, password, driver)
+		def sql = Sql.newInstance(url, user, password, driver)
 		List result = sql.rows(query)
 		sql.close()
 		if(! result) {
@@ -32,8 +32,7 @@ class ExampleSql {
 	}
 
 	Boolean execute( query ) {
-		def sql = ExampleSql.newInstance(url, user, password, driver)
-
+		def sql = Sql.newInstance(url, user, password, driver)
 		Boolean result = sql.execute(query)
 		sql.close()
 		return result
